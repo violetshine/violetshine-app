31c932ceab536c192ad36dd989abb9f847de84f9
Violet <me@violetshine.net>
2026-06-27 02:23:08 +0300

    fix: work around a bug causing 404 error to be empty

--- app/src/main/kotlin/Routing.kt
+++ app/src/main/kotlin/Routing.kt
@@ -12,17 +12,20 @@ import io.ktor.server.http.content.*
 import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
+  // Bug workaround: https://slack-chats.kotlinlang.org/t/8518192/hello-when-i-try-to-make-a-request-on-a-route-that-does-not-
+  // Without this, HTTP 404 does not work and a 404 results in an empty
+  // page being returned to the client with a 200 status code.
+  intercept(ApplicationCallPipeline.Fallback) {
+    if (call.isHandled) return@intercept
+    val status = call.response.status() ?: HttpStatusCode.NotFound
+    call.respond(status)
+  }
+
   routing {
     staticResources("/_", "static") {
       fallback { requestedPath, call ->
         call.respond(HttpStatusCode.NotFound)
       }
-    }
-    get("*") {
-      call.respond(HttpStatusCode.NotFound)
-    }
-    post("*") { 
-      call.respond(HttpStatusCode.NotFound)
     }
   }
 }
--- app/src/main/kotlin/plugins/StatusPages.kt
+++ app/src/main/kotlin/plugins/StatusPages.kt
@@ -2,11 +2,15 @@ package net.violetshine.app.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.NotFoundException
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 
 fun Application.configureStatusPages() {
   install(StatusPages) {
+    exception<NotFoundException> { call, cause ->
+      call.respondText(text = "404: Page Not Found", status = HttpStatusCode.NotFound)
+    }
     status(HttpStatusCode.NotFound) { call, status ->
       call.respondText(text = "404: Page Not Found", status = status)
     }
