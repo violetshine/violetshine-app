704038094cfb4e785dd8a37b59eccb52d47a17f6
Violet <me@violetshine.net>
2026-07-04 21:38:05 +0300

    feat: add helpful SVG tags that `kotlinx.html` doesn't expose

---
+++ app/src/main/kotlin/template/tag/svg/SVGTags.kt
@@ -0,0 +1,45 @@
+package net.violetshine.app.template.tag.svg
+
+import io.ktor.server.html.*
+import kotlinx.html.*
+
+// kotlinx.html doesn't appear to have some SVG tags so we have to manually add them ourselves
+class G(consumer: TagConsumer<*>) : 
+  HTMLTag("g", consumer, emptyMap(), inlineTag = true, emptyTag = false),
+    HtmlInlineTag
+
+fun SVG.g(block: G.() -> Unit = {}) {
+  G(consumer).visit(block)
+}
+
+class PATH(consumer: TagConsumer<*>) : 
+  HTMLTag("path", consumer, emptyMap(), inlineTag = true, emptyTag = false),
+    HtmlInlineTag
+
+fun G.path(block: PATH.() -> Unit = {}) {
+  PATH(consumer).visit(block)
+}
+
+class DEFS(consumer: TagConsumer<*>) : 
+  HTMLTag("defs", consumer, emptyMap(), inlineTag = true, emptyTag = false),
+    HtmlInlineTag
+    
+fun SVG.defs(block: DEFS.() -> Unit = {}) {
+  DEFS(consumer).visit(block)
+}
+
+class CLIPPATH(consumer: TagConsumer<*>) : 
+  HTMLTag("clipPath", consumer, emptyMap(), inlineTag = true, emptyTag = false),
+    HtmlInlineTag
+
+fun DEFS.clipPath(block: CLIPPATH.() -> Unit = {}) {
+  CLIPPATH(consumer).visit(block)
+}
+
+class RECT(consumer: TagConsumer<*>) : 
+  HTMLTag("rect", consumer, emptyMap(), inlineTag = true, emptyTag = false),
+    HtmlInlineTag
+
+fun CLIPPATH.rect(block: RECT.() -> Unit = {}) {
+  RECT(consumer).visit(block)
+}
